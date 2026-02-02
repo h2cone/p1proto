7527b4818540667a235cbcdbe4b2b1cbf94bee9c
h2cone <tw8ape@gmail.com>
2026-02-03 00:05:06 +0800

    feat(game): toggle BGM with input action

--- rust/src/game/mod.rs
+++ rust/src/game/mod.rs
@@ -1,3 +1,4 @@
+use godot::classes::{AudioStreamPlayer, Input};
 use godot::prelude::*;
 
 mod player_spawner;
@@ -13,15 +14,49 @@ pub use spawn_resolver::SpawnResolver;
 #[class(base=Node)]
 pub struct Game {
     base: Base<Node>,
+    bgm_player: OnReady<Gd<AudioStreamPlayer>>,
+    bgm_enabled: bool,
 }
 
 #[godot_api]
 impl INode for Game {
     fn init(base: Base<Node>) -> Self {
-        Self { base }
+        Self {
+            base,
+            bgm_player: OnReady::from_node("AudioStreamPlayer"),
+            bgm_enabled: false,
+        }
     }
 
     fn ready(&mut self) {
-        godot_print!("[Game] ready")
+        godot_print!("[Game] ready");
+
+        self.base_mut()
+            .set_process_mode(godot::classes::node::ProcessMode::ALWAYS);
+        self.base_mut().set_process(true);
+
+        // Default: BGM off unless player toggles it on.
+        self.bgm_player.stop();
+        self.bgm_enabled = false;
+    }
+
+    fn process(&mut self, _delta: f64) {
+        let input = Input::singleton();
+        if input.is_action_just_pressed("ui_bgm_toggle") {
+            self.toggle_bgm();
+        }
+    }
+}
+
+#[godot_api]
+impl Game {
+    fn toggle_bgm(&mut self) {
+        self.bgm_enabled = !self.bgm_enabled;
+        if self.bgm_enabled {
+            self.bgm_player.play();
+        } else {
+            self.bgm_player.stop();
+        }
+        godot_print!("[Game] bgm_enabled: {}", self.bgm_enabled);
     }
 }
